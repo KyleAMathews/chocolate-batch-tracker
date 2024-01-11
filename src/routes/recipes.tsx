cf07e41f7905bd6ce9815f801472eb71888daefc
Kyle Mathews <mathews.kyle@gmail.com>
2024-01-11 11:51:24 -0800

    Finally get all editing recipes working

--- src/routes/recipes.tsx
+++ src/routes/recipes.tsx
@@ -1,5 +1,6 @@
-import { Flex } from "@radix-ui/themes"
-import { Pencil2Icon } from "@radix-ui/react-icons"
+import { useState } from "react"
+import { Flex, Box } from "@radix-ui/themes"
+import { Pencil2Icon, PlusIcon } from "@radix-ui/react-icons"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -30,6 +31,7 @@ import {
 } from "@/components/ui/form"
 
 const ingredientSchema = z.object({
+  id: z.string().uuid(),
   name: z.string().min(1, { message: `Ingredient name is required.` }),
   percentage: z.preprocess(
     (val) => {
@@ -60,106 +62,151 @@ const formSchema = z.object({
   ingredients: z.array(ingredientSchema),
 })
 
-export function RecipeForm() {
+export function RecipeForm({ recipe, closeForm }) {
+  // react-hook-form overwrites the id in arrays...
+  if (recipe.id) {
+    recipe.recipe_ingredients = recipe.recipe_ingredients.map((i) => {
+      return {
+        ...i,
+        ingredient_id: i.id,
+      }
+    })
+  }
+
   const { db } = useElectric()!
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      recipeName: ``,
-      description: ``,
-      ingredients: [{ name: ``, percentage: 0 }],
+      recipeName: recipe?.name || ``,
+      description: recipe?.description || ``,
+      ingredients: recipe?.recipe_ingredients || [
+        { id: genUUID(), name: ``, percentage: `0` },
+      ],
+    },
+    values: {
+      recipeName: recipe?.name,
+      description: recipe?.description,
+      ingredients: recipe?.recipe_ingredients,
     },
   })
   const { register, control, handleSubmit, formState, reset } = form
   const { errors } = formState
   const values = form.getValues()
-  console.log({ formState, values })
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log({ values })
-    const recipe_id = genUUID()
-    console.log(
-      await db.recipes.create({
-        data: {
-          id: recipe_id,
-          description: values.description,
-          name: values.recipeName,
-        },
+
+    const recipe_id = recipe.id || genUUID()
+    await db.recipes.upsert({
+      create: {
+        id: recipe_id,
+        description: values.description,
+        name: values.recipeName,
+      },
+      update: {
+        description: values.description,
+        name: values.recipeName,
+      },
+      where: {
+        id: recipe_id,
+      },
+    })
+    await Promise.all(
+      values.ingredients.map((ingredient) => {
+        const id = ingredient.id || genUUID()
+        return db.recipe_ingredients.upsert({
+          create: {
+            id,
+            recipe_id,
+            name: ingredient.name,
+            percentage: ingredient.percentage,
+          },
+          update: {
+            recipe_id,
+            name: ingredient.name,
+            percentage: ingredient.percentage,
+          },
+          where: {
+            id,
+          },
+        })
       })
     )
-    console.log(
-      await Promise.all(
-        values.ingredients.map((ingredient) => {
-          return db.recipe_ingredients.create({
-            data: {
-              id: genUUID(),
-              recipe_id,
-              name: ingredient.name,
-              percentage: ingredient.percentage,
-            },
-          })
-        })
-      )
-    )
+    closeForm()
   }
 
   const { fields, append, remove } = useFieldArray({
     control,
     name: `ingredients`,
   })
-  console.log(`form,errors`, form, errors)
+  console.log(`form,errors, values`, form, errors, values)
 
   return (
-    <Form {...form}>
-      <form onSubmit={handleSubmit(onSubmit)} className="">
-        <Flex direction="column" gap="5">
-          <Flex direction="column" gap="3">
-            <FormField
-              control={control}
-              name="recipeName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Recipe Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter recipe name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="Describe your recipe" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </Flex>
-          <Flex direction="column" gap="3">
-            <h2 className="text-l font-semibold">Ingredients</h2>
-            <div className="text-sm">
-              Total:{` `}
-              {values.ingredients
-                .map((i) => i.percentage)
-                .reduce((a, b) => parseInt(a, 10) + parseInt(b, 10), 0)}
-              %
-            </div>
-            <div className="space-y-2">
-              <Flex gap="3">
+    <div className="py-6">
+      <h2 className="text-xl font-semibold mb-2">
+        {recipe.id ? `Edit Recipe` : `Add Recipe`}
+      </h2>
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)} className="">
+          <Flex direction="column" gap="5">
+            <Flex direction="column" gap="3">
+              <FormField
+                control={control}
+                name="recipeName"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Recipe Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter recipe name"
+                          type="text"
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )
+                }}
+              />
+              <FormField
+                control={control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Describe your recipe" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </Flex>
+            <Flex direction="column" gap="3">
+              <h2 className="text-l font-semibold">Ingredients</h2>
+              <div className="text-sm">
+                Total:{` `}
+                {values.ingredients
+                  .map((i) => i.percentage)
+                  .reduce((a, b) => {
+                    return parseInt(a, 10) + (b ? parseInt(b, 10) : 0)
+                  }, 0)}
+                %
+              </div>
+              <Flex gap="3" direction="column">
                 {fields.map((field, index) => {
                   return (
-                    <>
+                    <Flex gap="3">
+                      <input
+                        type="hidden"
+                        {...register(`ingredients.${index}.id`)}
+                      />
                       <Input
                         key={`name` + field.id} // important to include key with field's id
                         {...register(`ingredients.${index}.name`)}
@@ -174,34 +221,41 @@ export function RecipeForm() {
                       <Button
                         type="button"
                         variant="ghost"
-                        onClick={() => remove(index)}
+                        onClick={() => {
+                          remove(index)
+                          db.recipe_ingredients.delete({
+                            where: { id: field.ingredient_id },
+                          })
+                        }}
                       >
                         Remove
                       </Button>
-                    </>
+                    </Flex>
                   )
                 })}
               </Flex>
-            </div>
-            <div>
-              <Button
-                className="mt-2"
-                variant="outline"
-                onClick={() => append({ name: ``, percentage: 0 })}
-              >
-                Add New Ingredient
-              </Button>
-            </div>
+              <div>
+                <Button
+                  className="mt-2"
+                  variant="outline"
+                  onClick={() =>
+                    append({ id: genUUID(), name: ``, percentage: 0 })
+                  }
+                >
+                  Add New Ingredient
+                </Button>
+              </div>
+            </Flex>
           </Flex>
-        </Flex>
-        <div className="flex justify-end">
-          <Button className="mr-2" variant="outline">
-            Cancel
-          </Button>
-          <Button type="submit">Save</Button>
-        </div>
-      </form>
-    </Form>
+          <div className="flex justify-end">
+            <Button className="mr-2" variant="outline">
+              Cancel
+            </Button>
+            <Button type="submit">Save</Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   )
 }
 
@@ -217,20 +271,35 @@ Recipes.queries = queries
 
 export default function Recipes() {
   const location = useLocation()
+  const [editing, setEditing] = useState(null)
   const { recipes } = useElectricData(location.pathname + location.search)
-  console.log({ recipes })
   return (
     <Flex direction="column">
-      <h1 className="text-2xl font-bold">Chocolate Recipes</h1>
+      <Flex align="center" gap="3">
+        <h1 className="text-2xl font-bold">Chocolate Recipes</h1>
+        <PlusIcon
+          style={{ transform: `scale(1.2)`, cursor: `pointer` }}
+          onClick={() => setEditing({})}
+        />
+      </Flex>
       <div className="divide-y divide-gray-200">
         <div className="py-6">
           <h2 className="text-xl font-semibold mb-2">Recipes</h2>
           {recipes.length > 0 && (
-            <Flex gap="4">
+            <Flex gap="4" wrap="wrap">
               {recipes.map((recipe) => {
                 return (
-                  <Card className="relative">
-                    <div style={{ position: `absolute`, top: 8, right: 8 }}>
+                  <Card className="relative min-w-52">
+                    <div
+                      style={{
+                        position: `absolute`,
+                        top: 2,
+                        right: 2,
+                        cursor: `pointer`,
+                        padding: 8,
+                      }}
+                      onClick={() => setEditing(recipe)}
+                    >
                       <Pencil2Icon />
                     </div>
                     <CardHeader>
@@ -258,10 +327,13 @@ export default function Recipes() {
           )}
           {recipes.length === 0 && <div>Add your first recipe!</div>}
         </div>
-        <div className="py-6">
-          <h2 className="text-xl font-semibold mb-2">Add/Edit Recipe</h2>
-          <RecipeForm />
-        </div>
+        {editing && (
+          <RecipeForm
+            key={editing?.id || Math.random()}
+            recipe={editing}
+            closeForm={() => setEditing(null)}
+          />
+        )}
       </div>
     </Flex>
   )
