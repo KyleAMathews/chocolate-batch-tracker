227dbd2996e371942ade7b1f6b8a63498ef90606
Kyle Mathews <mathews.kyle@gmail.com>
2024-01-11 09:59:00 -0800

    form looking pretty

--- src/routes/recipes.tsx
+++ src/routes/recipes.tsx
@@ -1,4 +1,5 @@
 import { Flex } from "@radix-ui/themes"
+import { Pencil2Icon } from "@radix-ui/react-icons"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -32,11 +33,9 @@ const ingredientSchema = z.object({
   name: z.string().min(1, { message: `Ingredient name is required.` }),
   percentage: z.preprocess(
     (val) => {
-      console.log(val)
       // Attempt to convert the value to a number if it's a string
       if (typeof val === `string`) {
         const parsed = parseInt(val)
-        console.log(val)
         // Return the parsed number if it's a valid number, otherwise return undefined
         return isNaN(parsed) ? 0 : parsed
       }
@@ -48,7 +47,8 @@ const ingredientSchema = z.object({
         required_error: `Percentage amount is required.`,
         invalid_type_error: `Percentage amount must be a number.`,
       })
-      .min(1, { message: `Ingredient amount must be greater than zero.` })
+      .min(1, { message: `Ingredient percentage must be greater than zero.` })
+      .max(100, { message: `Ingredient percentage cannot be more than 100.` })
   ),
 })
 
@@ -73,6 +73,9 @@ export function RecipeForm() {
     },
   })
   const { register, control, handleSubmit, formState, reset } = form
+  const { errors } = formState
+  const values = form.getValues()
+  console.log({ formState, values })
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -109,76 +112,88 @@ export function RecipeForm() {
     control,
     name: `ingredients`,
   })
-  console.log({ form, formState })
+  console.log(`form,errors`, form, errors)
 
   return (
     <Form {...form}>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-        <div className="mb-4">
-          <FormField
-            control={control}
-            name="recipeName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Recipe Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter recipe name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Description</FormLabel>
-                <FormControl>
-                  <Textarea placeholder="Describe your recipe" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <div className="mb-4">
-          <Label>Ingredients</Label>
-          <div className="space-y-2">
-            <div className="grid grid-cols-3 gap-2">
-              {fields.map((field, index) => {
-                return (
-                  <>
-                    <Input
-                      key={`name` + field.id} // important to include key with field's id
-                      {...register(`ingredients.${index}.name`)}
-                      placeholder="Ingredient name"
-                    />
-                    <Input
-                      key={`percentage` + field.id} // important to include key with field's id
-                      type="number"
-                      {...register(`ingredients.${index}.percentage`)}
-                      placeholder="Percentage"
-                    />
-                    <Button
-                      type="button"
-                      variant="outline"
-                      onClick={() => remove(index)}
-                    >
-                      Remove
-                    </Button>
-                  </>
-                )
-              })}
+      <form onSubmit={handleSubmit(onSubmit)} className="">
+        <Flex direction="column" gap="5">
+          <Flex direction="column" gap="3">
+            <FormField
+              control={control}
+              name="recipeName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Recipe Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter recipe name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Describe your recipe" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </Flex>
+          <Flex direction="column" gap="3">
+            <h2 className="text-l font-semibold">Ingredients</h2>
+            <div className="text-sm">
+              Total:{` `}
+              {values.ingredients
+                .map((i) => i.percentage)
+                .reduce((a, b) => parseInt(a, 10) + parseInt(b, 10), 0)}
+              %
             </div>
-          </div>
-          <Button
-            className="mt-2"
-            onClick={() => append({ name: ``, percentage: 0 })}
-          >
-            Add New Ingredient
-          </Button>
-        </div>
+            <div className="space-y-2">
+              <Flex gap="3">
+                {fields.map((field, index) => {
+                  return (
+                    <>
+                      <Input
+                        key={`name` + field.id} // important to include key with field's id
+                        {...register(`ingredients.${index}.name`)}
+                        placeholder="Ingredient name"
+                      />
+                      <Input
+                        key={`percentage` + field.id} // important to include key with field's id
+                        type="number"
+                        {...register(`ingredients.${index}.percentage`)}
+                        placeholder="Percentage"
+                      />
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        onClick={() => remove(index)}
+                      >
+                        Remove
+                      </Button>
+                    </>
+                  )
+                })}
+              </Flex>
+            </div>
+            <div>
+              <Button
+                className="mt-2"
+                variant="outline"
+                onClick={() => append({ name: ``, percentage: 0 })}
+              >
+                Add New Ingredient
+              </Button>
+            </div>
+          </Flex>
+        </Flex>
         <div className="flex justify-end">
           <Button className="mr-2" variant="outline">
             Cancel
@@ -205,43 +220,41 @@ export default function Recipes() {
   const { recipes } = useElectricData(location.pathname + location.search)
   console.log({ recipes })
   return (
-    <div className="grid gap-6 md:gap-8 px-4 md:px-6">
-      <h1 className="text-2xl font-bold mb-4">Chocolate Recipes</h1>
+    <Flex direction="column">
+      <h1 className="text-2xl font-bold">Chocolate Recipes</h1>
       <div className="divide-y divide-gray-200">
         <div className="py-6">
           <h2 className="text-xl font-semibold mb-2">Recipes</h2>
           {recipes.length > 0 && (
-            <ul className="divide-y divide-gray-200">
+            <Flex gap="4">
               {recipes.map((recipe) => {
                 return (
-                  <Card>
-                    <Flex gap="3" direction="column">
-                      <CardHeader>
-                        <CardTitle className="underline">
-                          {recipe.name}
-                        </CardTitle>
-                        <CardDescription>{recipe.description}</CardDescription>
-                      </CardHeader>
-                      <CardContent>
-                        <ul>
-                          {recipe.recipe_ingredients.map((ingredient) => {
-                            return (
-                              <li>
-                                <span>
-                                  {ingredient.name} -{` `}
-                                  {ingredient.percentage}%
-                                </span>
-                              </li>
-                            )
-                          })}
-                        </ul>
-                        <span className="material-icons">edit</span>
-                      </CardContent>
-                    </Flex>
+                  <Card className="relative">
+                    <div style={{ position: `absolute`, top: 8, right: 8 }}>
+                      <Pencil2Icon />
+                    </div>
+                    <CardHeader>
+                      <CardTitle className="underline">{recipe.name}</CardTitle>
+                      <CardDescription>{recipe.description}</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <ul>
+                        {recipe.recipe_ingredients.map((ingredient) => {
+                          return (
+                            <li>
+                              <span>
+                                {ingredient.name} -{` `}
+                                {ingredient.percentage}%
+                              </span>
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    </CardContent>
                   </Card>
                 )
               })}
-            </ul>
+            </Flex>
           )}
           {recipes.length === 0 && <div>Add your first recipe!</div>}
         </div>
@@ -250,6 +263,6 @@ export default function Recipes() {
           <RecipeForm />
         </div>
       </div>
-    </div>
+    </Flex>
   )
 }
